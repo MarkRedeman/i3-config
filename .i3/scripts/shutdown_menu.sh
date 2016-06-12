41e66ff4a6e8a26814e36849c7bf22d099f48cf1
Mark Redeman <markredeman@gmail.com>
2016-06-12 16:23:43 +0200

    Make lock script posix compatible

--- .i3/scripts/shutdown_menu.sh
+++ .i3/scripts/shutdown_menu.sh
@@ -1,24 +1,40 @@
-#!/usr/bin/zsh
+#!/bin/sh
 
 main() {
-    readonly options=("1. lock
+    readonly options="1. lock
 2. logout
 3. suspend
 4. restart
-5. shutdown")
+5. shutdown"
 
     prompt() {
         printf "%s\n" "$@" | rofi -dmenu -i -p "Menu: "
     }
 
-    case "$(bc -q  <<< "$(prompt "${options[@]}") + 1")" in
+    # This function returns the given integer + 1 if the input
+    # is not empty and returns 0 if it is not set
+    sanitize() {
+        readonly possibleInt=$1
+
+        if [ -z "$possibleInt" ]
+        then
+            echo 0
+        else
+            echo "$possibleInt + 1" | bc -q
+        fi
+    }
+
+    readonly selected=$(sanitize "$(prompt "$options")")
+
+    case $selected in
         1) lock ;;
         2) i3-msg exit ;;
         3) systemctl suspend ;;
         4) systemctl reboot ;;
         5) systemctl poweroff ;;
+        *) exit ;;
     esac
 }
 
-main
+main "$@"
 exit 0
